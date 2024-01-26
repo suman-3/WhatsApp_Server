3025eb8281576016ab935155fbfd47955ccb8f6a
Suman Mondal <mondalsuman97322@gmail.com>
2024-01-27 04:13:15 +0530

    first commit

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const server = app.listen(process.env.PORT, () => {
 });
 const io = new Server(server, {
   cors: {
-    // origin: "http://localhost:3000",
-    origin: "https://chat-app-three-iota.vercel.app",
+    origin: "http://localhost:3000",
+    // origin: "https://chat-app-three-iota.vercel.app",
   },
 });
 
